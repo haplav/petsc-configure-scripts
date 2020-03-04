17cbd0b53c195c37206c59b00db0b055fc2b5040
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2020-03-04 10:39:16 +0100

    new file:   arch-osx-dbg.py

---
+++ arch-osx-dbg.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+
+configure_options = [
+  '--with-cc=clang',
+  '--with-cxx=clang++',
+  #'--with-fc=gfortran', # https://brew.sh/
+  '--with-fc=0',
+  'COPTFLAGS=-g -O',
+  'FOPTFLAGS=-g -O',
+  'CXXOPTFLAGS=-g -O',
+  '--download-make=1',
+  '--download-cmake=1',
+  '--download-f2cblaslapack=1',
+  '--download-ctetgen=1',
+  '--download-hdf5=1',
+  '--download-mpich=1',
+  '--download-mpich-device=ch3:nemesis', #for some reason runex174_2_elemental takes very long with ch3:p4
+  '--download-metis=1',
+  '--download-parmetis=1',
+  '--download-ptscotch',
+  '--download-libpng=1',
+  '--download-libjpeg=1',
+  '--with-zlib=1',
+  ]
+
+if __name__ == '__main__':
+  import sys,os
+  sys.path.insert(0,os.path.abspath('config'))
+  import configure
+  configure.petsc_configure(configure_options)
