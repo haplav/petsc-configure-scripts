2155602f222026cc9a5599b28c85bb0f3865612d
Vaclav Hapla <vaclav.hapla@erdw.ethz.ch>
2020-07-16 18:26:19 +0200

    update arch-osx-dbg-conda.py

--- arch-osx-dbg-conda.py
+++ arch-osx-dbg-conda.py
@@ -4,21 +4,35 @@
 # conda install cython numpy hdf5 make mpich mkl c-compiler fortran-compiler valgrind yaml
 
 import os
-CONDA_ENV="/opt/miniconda3/envs/petsc"
+
+CONDA_PREFIX = os.getenv('CONDA_PREFIX')
+CPPFLAGS = os.getenv('CPPFLAGS')
+FFLAGS  = '-g -O0 -pipe -Wall -Wno-strict-aliasing -fstack-protector-strong'
+CFLAGS = FFLAGS + ' -Wwrite-strings -Wno-unknown-pragmas'
+CXXFLAGS = CFLAGS + ' -fvisibility=hidden'
+
 configure_options = [
-  'COPTFLAGS=-g -O',
-  'FOPTFLAGS=-g -O',
-  'CXXOPTFLAGS=-g -O',
-  '--download-petsc4py',
+  'AR=' + os.getenv('AR'),
+  'CPPFLAGS=' + CPPFLAGS,
+  'CXXPPFLAGS=' + CPPFLAGS,
+  'LDFLAGS=-Wl,-headerpad_max_install_names -Wl,-dead_strip_dylibs -Wl,-rpath,%s/lib -L%s/lib' % (CONDA_PREFIX, CONDA_PREFIX),
+  'RANLIB=' + os.getenv('RANLIB'),
+  'CFLAGS=' + CFLAGS,
+  'CXXFLAGS=' + CXXFLAGS,
+  'FFLAGS=' + FFLAGS,
+  #'--download-petsc4py',
   '--download-sowing',
-  '--with-blaslapack-dir=' + CONDA_ENV,
-  '--with-hdf5-dir=' + CONDA_ENV,
-  '--with-make-dir=' + CONDA_ENV,
-  '--with-mpi-dir=' + CONDA_ENV,
-  '--with-python-exec=%s/bin/python' % CONDA_ENV,
+  '--with-blaslapack-dir=' + CONDA_PREFIX,
+  '--with-debugging=1',
+  '--with-hdf5-dir=' + CONDA_PREFIX,
+  '--with-make-dir=' + CONDA_PREFIX,
+  '--with-mpi-dir=' + CONDA_PREFIX,
+  '--with-python-dir=' + CONDA_PREFIX,
+  '--with-python-exec=%s/bin/python' % CONDA_PREFIX,
   '--with-shared-libraries=1',
-  '--with-valgrind-dir=' + CONDA_ENV,
-  '--with-yaml-dir=' + CONDA_ENV,
+  '--with-single-library=1',
+  '--with-valgrind-dir=' + CONDA_PREFIX,
+  '--with-yaml-dir=' + CONDA_PREFIX,
   '--with-x=0',
 ]
 
